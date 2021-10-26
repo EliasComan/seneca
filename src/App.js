b5999a24da06fc2c8b52c2d31e4f8b84f43e2bb9
EliasComan <comanelias5@gmail.com>
2021-10-26 16:16:37 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import './App.css';
-import CartWidget from './Components/CartWidget/CartWidget';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
-import Contador from './Components/contador/Contador.jsx'
+import Contador from './Components/contador/Contador.jsx';
+import NavBar from './Components/navbar/navBar.jsx';
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <p className="navbar-brand" >Seneca</p>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <p className="nav-link active" aria-current="page" >Home</p>
-              </li>
-              <li className="nav-item">
-                <p className="nav-link" >Shop</p>
-              </li>
-              <li className="nav-item">
-                <p className="nav-link" >About us</p>
-              </li>
-            </ul>
-            <span className="navbar-text">
-             <CartWidget/>
-            </span>
-          </div>
-        </div>
-      </nav>
-    </header>
- 
-    <ItemListContainer greeting='Bienvenidos!' />
-    <Contador cantidad={10}/>
+      <NavBar/>
+      <ItemListContainer greeting='Bienvenidos!' />
+      <Contador cantidad={10}/>
     </div>
   );
 }
---
+++ src/Components/navbar/navBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import CartWidget from '../CartWidget/CartWidget'
+
+
+const NavBar = () =>{
+    return (
+        <>
+        <header className="App-header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <p className="navbar-brand" >Seneca</p>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <p className="nav-link active" aria-current="page" >Home</p>
+              </li>
+              <li className="nav-item">
+                <p className="nav-link" >Shop</p>
+              </li>
+              <li className="nav-item">
+                <p className="nav-link" >About us</p>
+              </li>
+            </ul>
+            <span className="navbar-text">
+             <CartWidget/>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </header>
+    </>
+    
+)}
+export default NavBar;
