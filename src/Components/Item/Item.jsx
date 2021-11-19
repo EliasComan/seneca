637f270db074456f4792fa9faf13af328fa19e99
EliasComan <comanelias5@gmail.com>
2021-11-19 09:58:04 -0300

    commit

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,17 +6,18 @@ const Item = ({nombre , imagen, id,categoria}) => {
    
     return(
       
-    
-    <div className="col">
-    <div className="card h-100">
-      <img src={`${imagen}`} className="card-img-top" alt="foto del nft"/>
-      <div className="card-body">
-        <h5 className="card-title">{nombre}</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <Link to={`/${categoria}/${id}` } ><button type="button" className="btn btn-dark">Comprar</button></Link>
+      <div className="col">
+        <Link  to={`/${categoria}/${id}` } className='card-link' >
+      <div className="card h-100">
+        <img src={`${imagen}`} className="card-img-top" alt="foto del nft"/>
+        <div className="card-body">
+          <h5 className="card-title">{nombre}</h5>
+          <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <Link to={`/${categoria}/${id}` } ><button type="button" className="btn btn-dark">Comprar</button></Link>
+        </div>
       </div>
-    </div>
-    </div>
+    </Link>
+      </div>
 )}
 
 export default Item
