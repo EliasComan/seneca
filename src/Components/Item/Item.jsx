1c7c00d7445f48bd863a91303bc9453963678db2
EliasComan <comanelias5@gmail.com>
2021-11-19 10:00:18 -0300

    commit

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -12,7 +12,7 @@ const Item = ({nombre , imagen, id,categoria}) => {
         <img src={`${imagen}`} className="card-img-top" alt="foto del nft"/>
         <div className="card-body">
           <h5 className="card-title">{nombre}</h5>
-          <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p className="card-text">Texto de prueba.</p>
           <Link to={`/${categoria}/${id}` } ><button type="button" className="btn btn-dark">Comprar</button></Link>
         </div>
       </div>
