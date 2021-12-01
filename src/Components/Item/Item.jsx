94e4df0a046c5c47fda2e9bb2b3b9103609d361b
EliasComan <comanelias5@gmail.com>
2021-12-01 20:53:25 -0300

    commit

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -12,7 +12,6 @@ const Item = ({nombre , imagen, id,categoria }) => {
             <img src={`${imagen}`} className="card-img-top" alt="foto del nft"/>
             <div className="card-body">
               <h5 className="card-title">{nombre}</h5>
-              <p className="card-text">Texto de prueba.</p>
               <Link to={`/${categoria}/${id}` } ><button type="button" className="btn btn-dark">Comprar</button></Link>
             </div>
           </div>
--- src/Components/cart/Cart.jsx
+++ src/Components/cart/Cart.jsx
@@ -1,15 +1,84 @@
-import React, { useContext , memo} from 'react';
+import React, { useContext , memo, useState} from 'react';
 import {CartContext} from '../../CartContext/CartContextProvider';
 import {Link }from 'react-router-dom'
 import './cart.css'
+import { getFirestore } from '../../services/getFire';
 
 const Cart = memo (()  => {
 
-    const {  clearCart, cart, deteleItem, precioCart, cantItem } = useContext(CartContext);
-    return(
+    const {  clearCart, cart, deteleItem, precioCart } = useContext(CartContext);
+    const [ buyer, setBuyer] = useState({})
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [buyid, setbuyId] = useState(null)
+    const [telefono, setTelefono] = useState(0)
+
+ 
+    
+    
+    const generarOrden = (e) => {
+        e.preventDefault();
+        
+        const compra = cart.map(i => {
+            const cantidad = i.cantidad
+            const precio = Math.round(100 * (i.precio * i.cantidad))/100;
+            const nombre = i.nombre;
+            return{cantidad, precio, nombre}
+
+        })
+
+        const comprador = {name, email, telefono}
+
+      setBuyer({comprador,compra})  
+
+      const getFb = getFirestore();
+      const ordenes = getFb.collection('ordenes').add(buyer)
+             ordenes .then(res => setbuyId(res.id))
+              ordenes.catch(err => console.log(err))
+    }
+    
+  
+            return(
         <>
-            { cart.length > 0 &&<button className="btn btn-dark" onClick={clearCart}> clearCart</button>}
-            {Math.round(100 * precioCart ()) / 100 > 0 && <h1>Precio total: { Math.round(100 * precioCart ()) / 100 } ETH </h1> }
+            { cart.length > 0 &&
+            <div className="cart-top">
+            <button className="btn btn-dark" onClick={clearCart}> Limpiar carrito</button>
+            <h1>Precio total: { Math.round(100 * precioCart ()) / 100 } ETH </h1> 
+            <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">Finalizar compra </button>
+            </div>
+            }
+                    
+                    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="mb-3">
+                                        <label  className="form-label">Nombre.</label>
+                                        <input type="email" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" id="nombre" aria-describedby="emailHelp"/>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label  className="form-label">Email.</label>
+                                        <input type="text"  value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" id="email"/>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label  className="form-label">Telefono.</label>
+                                        <input type="text" className="form-control" value={telefono} onChange={(e) => setTelefono(e.target.value)} id="telefono"/>
+                                    </div>
+                                    <button onClick={   generarOrden }  className="btn btn-dark">comprar</button>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                               {  buyid && <p style={ {marginRight:'50%'}}>Su id de compra es : {buyid} </p> }
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
             
             <div className="row row-cols-1 row-cols-md-3 g-4">  
                 { cart.length > 0 ?
@@ -22,10 +91,10 @@ const Cart = memo (()  => {
                                     <div className="col-md-4 ">
                                     <img src={i.imagen} className="img-fluid rounded-start" alt=""/>
                                     </div>
-                                    <div class="col-md-8 ">
+                                    <div className="col-md-8 ">
                                     <div className="card-body ">
                                         <h5 className="card-title">{i.nombre}</h5>
-                                        <p className="card-text"> precio : {Math.round(100 * (i.precio * cantItem()))/100} ETH</p>
+                                        <p className="card-text"> precio : {Math.round(100 * (i.precio * i.cantidad))/100} ETH</p>
                                         <p className="card-text"><small className="text-muted">Cantidad : {i.cantidad}</small></p>
                                         <button  onClick={() => deteleItem(i.id)} className="btn btn-light">eliminar</button>
                                     </div>
