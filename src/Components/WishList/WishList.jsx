89cd4eefcfb5d76ada4ff9ebb5c4cf59184dd199
EliasComan <comanelias5@gmail.com>
2021-12-10 15:45:32 -0300

    mensaje wishlist

--- src/Components/WishList/WishList.jsx
+++ src/Components/WishList/WishList.jsx
@@ -1,36 +1,42 @@
 import React,{useContext, useEffect, useState} from 'react';
 import { getFirestore } from '../../services/getFire';
 import {SessionContext} from '../../SessionContext/SessionContextProvider';
+import './Wishlist.css'
 
 
 
 const WishList = () =>{
-    const [items, setItems] = useState([])
+    const [items, setItems] = useState()
     const {  user, deleteFromWishList } = useContext(SessionContext)
     useEffect(()=>{
          if (user) {
             const getfb = getFirestore().collection('wishlist').doc(user).get()
-             getfb.then( res =>{setItems(   res.data().items ); console.log(res.data().items);} )
+             getfb.then( res =>{setItems(   res.data().items )} )
         }
     },[user,items])
 
     return(
-        <>{ 
-         items.map((i, index) => {return(
-         <div key={index} className="card mb-3 text-white bg-dark" style={{maxWidth: '18rem'}} >
-            <div className="row g-0">
-                <div className="col-md-4 ">
-                <img src={i.item.imagen} className="img-fluid rounded-start" alt=""/>
-                </div>
-                <div className="col-md-8 ">
-                <div className="card-body ">
-                    <h5 className="card-title">{i.item.nombre}</h5>
-                    <button  onClick={ () =>deleteFromWishList( i.item.nombre,i.item.imagen, user)} className="btn btn-light">eliminar</button>
-                </div>
+        <>{ items.legnt > 0 ?
+            items.map((i, index) => {return(
+            <div key={index} className="card mb-3 text-white bg-dark" style={{maxWidth: '18rem'}} >
+                <div className="row g-0">
+                    <div className="col-md-4 ">
+                    <img src={i.item.imagen} className="img-fluid rounded-start" alt=""/>
+                    </div>
+                    <div className="col-md-8 ">
+                    <div className="card-body ">
+                        <h5 className="card-title">{i.item.nombre}</h5>
+                        <button  onClick={ () =>deleteFromWishList( i.item.nombre,i.item.imagen, user)} className="btn btn-light">eliminar</button>
+                    </div>
+                    </div>
                 </div>
             </div>
-         </div>
-       )})}</>
+       )})
+       :
+       <div className='vacio'>
+       <h1>Aun no hay nada aqui</h1>
+       </div>
+       }</>
 
 
 
